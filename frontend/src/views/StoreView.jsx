b443416c905ae2d32bfef19ebd7006d155a7e678
Estigma9 <esduro@gmail.com>
2023-10-25 19:57:04 -0500

    test1

--- frontend/src/views/StoreView.jsx
+++ frontend/src/views/StoreView.jsx
@@ -1,10 +1,12 @@
 import StoreCarousel from '../components/StoreCarousel';
+import Counter from '../components/Counter';
 import TheFooter from '../components/TheFooter';
 
 function StoreView () {
     return (
         <div>           
             <StoreCarousel/>
+            <Counter/>
             <TheFooter></TheFooter>
         </div>
     );
